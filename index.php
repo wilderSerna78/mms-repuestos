b37b595844cec31325fd5637a238763ba33f085a
Mauricio Serna <112326618+mauser78@users.noreply.github.com>
2024-07-27 15:14:17 -0500

    Inicio pagina de mms-repuestos de motos.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MMS-Repuestos | Inicio</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h1>Proyecto MMS-Repuestos</h1>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
